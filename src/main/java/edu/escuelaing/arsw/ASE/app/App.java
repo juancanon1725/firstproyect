664ef1bfdc41643e62738df7923088c7824a68a4
unknown <juan.poveda-ca@labinfo.is.escuelaing.edu.co>
2024-06-11 12:02:26 -0500

    Primera versión del proyecto

---
+++ src/main/java/edu/escuelaing/arsw/ASE/app/App.java
@@ -0,0 +1,12 @@
+package edu.escuelaing.arsw.ASE.app;
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
